cf81bd25491f0e852b4c6edf4571d02b45e36b86
ibrahimBougaoua <ibrahimBougaoua@users.noreply.github.com>
2024-06-15 21:06:33 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace NumberToWord\NumberToWords\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use NumberToWord\NumberToWords\NumberToWordsServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
